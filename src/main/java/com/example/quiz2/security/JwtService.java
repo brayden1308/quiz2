7c874d8c96e9895e5815ac54a6a7249a1d06f314
brayden1308 <braydenvargas13@GMAIL.COM>
2026-04-14 11:09:52 -0500

    JWT implementado

--- src/main/java/com/example/quiz2/security/JwtService.java
+++ src/main/java/com/example/quiz2/security/JwtService.java
@@ -13,7 +13,7 @@ public class JwtService {
     private final Key key = Keys.hmacShaKeyFor(
             "mi_clave_secreta_super_segura_1234567890".getBytes()
     );
-
+  //jwt funcional
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username) // ✔ correcto
